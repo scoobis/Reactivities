e785d14607604fd55cd99735f1119c3365179eff
scoobis <gabriel.bjorlin@gmail.com>
2020-06-06 23:00:26 +0200

    init

---
+++ Persistance/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistance
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Value> Values { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder) {
+            builder.Entity<Value>().HasData(
+                new Value {id = 1, name = "values 101"},
+                new Value {id = 2, name = "values 102"},
+                new Value {id = 3, name = "values 103"}
+            );
+        }
+    }
+}
